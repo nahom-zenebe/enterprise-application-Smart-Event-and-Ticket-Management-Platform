6d2616aab9fc3ba1ff3f1c6228d8ae1445a26567
astroker00 <astroker920@gmail.com>
2025-12-29 07:41:37 +0300

    event-list components

--- Frontend/src/app/pages/attendee/event-list/event-list.component.ts
+++ Frontend/src/app/pages/attendee/event-list/event-list.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AttendeeService } from '../attendee.services';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './event-list.component.html',
   styleUrls: ['./event-list.component.css']
 })
-export class EventListComponent {
-  events = this.service.getEvents();
-  constructor(private service: AttendeeService, private router: Router) {}
-  openEvent(id: number) {
+export class EventListComponent implements OnInit {
+
+  events: any[] = [];
+
+  constructor(
+    private service: AttendeeService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.events = this.service.getEvents();
+  }
+
+  openEvent(id: number): void {
     this.router.navigate(['/attendee/events', id]);
   }
 }
--- Frontend/src/app/pages/attendee/my-tickets/my-tickets.component.ts
+++ Frontend/src/app/pages/attendee/my-tickets/my-tickets.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AttendeeService } from '../attendee.service';
+import { AttendeeService } from '../attendee.services';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './my-tickets.component.html',
   styleUrls: ['./my-tickets.component.css']
 })
-export class MyTicketsComponent {
-  tickets = this.service.getMyTickets();
+export class MyTicketsComponent implements OnInit {
 
-  constructor(private service: AttendeeService, private router: Router) {}
+  tickets: any[] = [];
 
-  viewTicket(id: number) {
+  constructor(
+    private service: AttendeeService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.tickets = this.service.getMyTickets();
+  }
+
+  viewTicket(id: number): void {
     this.router.navigate(['/attendee/ticket', id]);
   }
 }
